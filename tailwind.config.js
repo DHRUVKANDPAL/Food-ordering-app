44793f3efb54341f97632c17cda1bce8b79055f8
DHRUVKANDPAL <dhruvkandpal172@gmail.com>
2024-06-12 11:09:37 +0530

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
     extend: {
       backgroundImage: {
         'hero': "url('https://media.cntraveler.com/photos/61e886f19580ae68b5219023/master/pass/Oji%20Seichi%20Toronto_Oji%20Food-Food%20Spread_Roberto%20Caruso.jpg')",
-        'hero2':"url('https://t3.ftcdn.net/jpg/02/48/92/96/360_F_248929619_JkVBYroM1rSrshWJemrcjriggudHMUhV.jpg')"
+        'hero2':"url('https://wallpapers.com/images/featured/food-photography-background-iyvy1e8yztvry3g8.jpg')"
       }
     },
     
